12d819228ac7769c1d7e8d0d3843f029829b3e42
matheusrsd2 <matheus.santos@univem.edu.br>
2020-12-02 06:55:04 -0300

    Migration Init

---
+++ Migrations/20201202095417_Init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaVirtualCsharp.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Sexo = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clientes");
+        }
+    }
+}
